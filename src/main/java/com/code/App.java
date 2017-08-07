a93aedd6dcab96022c3d957e9014e37d7095e0a2
skbind <san.kbind@gmail.com>
2017-08-08 00:18:46 +0530

    maven demo project

---
+++ src/main/java/com/code/App.java
@@ -0,0 +1,8 @@
+package com.code;
+
+public class App {
+	public String Sample(){
+		return "sample";
+	}
+
+}
---
+++ src/test/java/com/code/AppTest.java
@@ -0,0 +1,14 @@
+package com.code;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
